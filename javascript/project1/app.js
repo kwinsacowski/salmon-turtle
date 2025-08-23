85e158288400c110d3e7c1eb124087ba6ffac098
Kayla Salmon <kayla.wienczkowski@yahoo.com>
2025-08-23 14:51:28 -0400

    break

---
+++ javascript/project1/app.js
@@ -0,0 +1,29 @@
+let fruits = ["apple", "banana", "cherry"]
+console.log(fruits[1]);
+fruits.splice[1, 0, "blueberry"];
+
+fruits.push ("orange");
+fruits.pop ();
+fruits.unshift("grape")
+fruits.shift();
+
+let numbers = [3, 4, 9, 12];
+console.log(numbers[0] + numbers[3]);
+console.log(numbers.length);
+numbers.push(15, 18);
+console.log(numbers.length);
+
+numbers[1] = null;
+console.log(numbers);
+let colors = ["red", "blue", "green"];
+colors[2] = "black";
+console.log(colors);
+
+let favorites = ["mac and cheese", "cheese ravioli", "gook"];
+favorites.push("burgers");
+console.log("I like", favorites[0], "and", favorites[3]);
+
+
+
+
+
